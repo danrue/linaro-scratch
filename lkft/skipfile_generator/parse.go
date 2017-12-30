738bef820c17513f0a963224acde6ac62d9e27fe
Dan Rue <dan.rue@linaro.org>
2017-12-30 12:20:05 -0600

    Parsing and skipfile writing is working

--- lkft/skipfile_generator/parse.go
+++ lkft/skipfile_generator/parse.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"gopkg.in/yaml.v2"
+	"os"
+	"log"
 
 )
 
@@ -13,8 +15,10 @@ func check(e error) {
 	}
 }
 
+// Declare a custom StringArray type that can be used for parsing
+// yaml fields that may be a string, or an array.
+// See https://github.com/go-yaml/yaml/issues/100
 type StringArray []string
-
 func (a *StringArray) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	var multi []string
 	err := unmarshal(&multi)
@@ -31,11 +35,21 @@ func (a *StringArray) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	return nil
 }
 
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a || b == "all" {
+			return true
+		}
+	}
+	return false
+}
+
+// Structure of the yaml skipfile
 type Skipfile struct {
 	Matrix struct {
-		Environments []string
 		Boards []string
 		Branches []string
+		Environments []string
 	}
 	Skiplist []struct {
 		Reason string
@@ -55,7 +69,29 @@ func main() {
 	var skips Skipfile
 	err = yaml.Unmarshal(buf, &skips)
 	check(err)
-	fmt.Printf("%v\n", skips)
+	//fmt.Printf("%v\n", skips)
 
-
+	for _, board := range skips.Matrix.Boards {
+		for _, branch := range skips.Matrix.Branches {
+			for _, environment := range skips.Matrix.Environments {
+				skipfile := fmt.Sprintf("skiplist_%s_%s_%s", board, branch, environment)
+				f, err := os.OpenFile(skipfile, os.O_RDWR|os.O_CREATE, 0644)
+				if err != nil {
+					log.Fatal(err)
+				}
+				for _, skip := range skips.Skiplist {
+					if stringInSlice(board, skip.Boards) &&
+					   stringInSlice(branch, skip.Branches) &&
+					   stringInSlice(environment, skip.Environments) {
+						for _, test := range skip.Tests {
+							f.WriteString(fmt.Sprintf("%s\n", test))
+						}
+					}
+				}
+				if err := f.Close(); err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+	}
 }
