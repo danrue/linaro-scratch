05c8fb217dc9d72b68d1fca6d34676fa855ec634
Dan Rue <dan.rue@linaro.org>
2018-01-05 16:37:51 -0600

    Generate builds page

---
+++ lkft/doc/generate_builds.py
@@ -0,0 +1,46 @@
+from tabulate import tabulate
+
+boards = [
+    # pretty name, architecture, jenkins MACHINE name
+    ('hikey', 'arm64', 'hikey'),
+    ('x15', 'arm', 'am57xx-evm'),
+    ('dell', 'x86_64', 'intel-core2-32'),
+]
+branches = [
+    # pretty name, squad name, jenkins name
+    ('4.14 LTS', 'stable-4.14-oe', 'stable-4.14'),
+    ('4.14-rc LTS', 'stable-rc-4.14-oe', 'stable-rc-4.14'),
+    ('4.9 LTS', 'stable-4.9-oe', 'stable-rc-4.9'),
+    ('4.9-rc LTS', 'stable-rc-4.9-oe', 'stable-rc-4.9'),
+]
+
+#def markdown(table, headers):
+#    buf = ""
+#    buf += "|{}|".format("|".join(headers))
+#    buf += "|{}}
+#    for row in table:
+#        buf +
+
+
+table = []
+for board in boards:
+    row = []
+    row.append(board[0]) # board pretty name is first column
+    for branch in branches:
+        # Append a row to the table
+        row.append(
+            "![{}]({})<br />![{}]({})".format( # markdown
+            #'<a href="{}"><img src="{}" /></a><br /><a href="{}"><img src="{}" /></a>'.format( # html
+                "https://ci.linaro.org/view/lkft/job/openembedded-lkft-linux-{}/DISTRO=rpb,MACHINE={},label=docker-lkft/".format(branch[2], board[2]),
+                "https://ci.linaro.org/buildStatus/icon?job=openembedded-lkft-linux-{}/DISTRO=rpb,MACHINE={},label=docker-lkft".format(branch[2], board[2]),
+                "https://qa-reports.linaro.org/lkft/linux-{}/".format(branch[1]),
+                "squad_favicon.png",
+            )
+        )
+    table.append(row)
+
+headers = ['Board']
+for branch in branches:
+    headers.append(branch[0])
+
+print(tabulate(table, headers=headers, tablefmt="html"))
