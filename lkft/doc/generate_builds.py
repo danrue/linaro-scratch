216eed55252b709415ce135d3cbd582b8581dcfa
Dan Rue <dan.rue@linaro.org>
2018-01-08 16:29:15 -0600

    Try using objects for build and board

--- lkft/doc/generate_builds.py
+++ lkft/doc/generate_builds.py
@@ -1,41 +1,71 @@
 from tabulate import tabulate
 
+class Board(object):
+    def __init__(self, name, architecture, jenkins_name):
+        self.name = name
+        self.architecture = architecture
+        self.jenkins_name = jenkins_name
+
+class Branch(object):
+    def __init__(self, name, squad_name, jenkins_name):
+        self.name = name
+        self.squad_name = squad_name
+        self.jenkins_name = jenkins_name
+
+class BoardxBranch(object):
+    def __init__(self, board, branch):
+        self.board = board
+        self.Branch = branch
+
+    def jenkins_badge_url(self):
+        return "https://ci.linaro.org/buildStatus/icon?job=openembedded-lkft-linux-{}/DISTRO=rpb,MACHINE={},label=docker-lkft".format(branch.jenkins_name, board.jenkins_name)
+
+    def jenkins_build_url(self):
+        return "https://ci.linaro.org/view/lkft/job/openembedded-lkft-linux-{}/DISTRO=rpb,MACHINE={},label=docker-lkft/".format(branch.jenkins_name, board.jenkins_name),
+
+    def squad_badge_url(self):
+        return "squad_favicon.png"
+    def squad_project_url(self):
+        return "https://qa-reports.linaro.org/lkft/linux-{}/".format(branch.squad_name)
+
+
 boards = [
     # pretty name, architecture, jenkins MACHINE name
-    ('hikey', 'arm64', 'hikey'),
-    ('x15', 'arm', 'am57xx-evm'),
-    ('juno', 'arm64', 'juno'),
-    ('dell', 'x86_64', 'intel-core2-32'),
+    Board('hikey', 'arm64', 'hikey'),
+    Board('x15', 'arm', 'am57xx-evm'),
+    Board('juno', 'arm64', 'juno'),
+    Board('dell', 'x86_64', 'intel-core2-32'),
 ]
 branches = [
     # pretty name, squad name, jenkins name
-    ('4.14 LTS', 'stable-4.14-oe', 'stable-4.14'),
-    ('4.14-rc LTS', 'stable-rc-4.14-oe', 'stable-rc-4.14'),
-    ('4.9 LTS', 'stable-4.9-oe', 'stable-rc-4.9'),
-    ('4.9-rc LTS', 'stable-rc-4.9-oe', 'stable-rc-4.9'),
-    ('4.4 LTS', 'stable-4.4-oe', 'stable-rc-4.4'),
-    ('4.4-rc LTS', 'stable-rc-4.4-oe', 'stable-rc-4.4'),
+    Branch('4.14 LTS', 'stable-4.14-oe', 'stable-4.14'),
+    Branch('4.14-rc LTS', 'stable-rc-4.14-oe', 'stable-rc-4.14'),
+    Branch('4.9 LTS', 'stable-4.9-oe', 'stable-rc-4.9'),
+    Branch('4.9-rc LTS', 'stable-rc-4.9-oe', 'stable-rc-4.9'),
+    Branch('4.4 LTS', 'stable-4.4-oe', 'stable-rc-4.4'),
+    Branch('4.4-rc LTS', 'stable-rc-4.4-oe', 'stable-rc-4.4'),
 ]
 
 table = []
 for board in boards:
     row = []
-    row.append(board[0]) # board pretty name is first column
-    row.append(board[1]) # board architecture is second column
+    row.append(board.name)
+    row.append(board.architecture)
     for branch in branches:
+        rowdata = BoardxBranch(board, branch)
         # Append a row to the table
         row.append(
             '<a href="{}"><img src="{}" /></a><br /><a href="{}"><img src="{}" /></a>'.format( # html
-                "https://ci.linaro.org/view/lkft/job/openembedded-lkft-linux-{}/DISTRO=rpb,MACHINE={},label=docker-lkft/".format(branch[2], board[2]),
-                "https://ci.linaro.org/buildStatus/icon?job=openembedded-lkft-linux-{}/DISTRO=rpb,MACHINE={},label=docker-lkft".format(branch[2], board[2]),
-                "https://qa-reports.linaro.org/lkft/linux-{}/".format(branch[1]),
-                "squad_favicon.png",
+            rowdata.jenkins_build_url(),
+            rowdata.jenkins_badge_url(),
+            rowdata.squad_project_url(),
+            rowdata.squad_badge_url(),
             )
         )
     table.append(row)
 
 headers = ['Board', 'Architecture']
 for branch in branches:
-    headers.append(branch[0])
+    headers.append(branch.name)
 
 print(tabulate(table, headers=headers, tablefmt="html"))
