8506e89c8722f78c320edeb6c3cf2839180c4f0b
Dan Rue <dan.rue@linaro.org>
2018-05-18 14:38:31 -0500

    Deal with multi-reports by trimming sig
    Signed-off-by: Dan Rue <dan.rue@linaro.org>

--- qa-reports/email_api/generate_lts_report.py
+++ qa-reports/email_api/generate_lts_report.py
@@ -11,18 +11,10 @@ branch = sys.argv[1]
 if branch not in ['4.4', '4.9', '4.14', '4.16']:
     usage()
 
-projects = {
-    'linaro-hikey-stable-rc-4.4-oe': 'https://qa-reports.linaro.org/api/projects/34/',
-    'linux-stable-rc-4.4-oe': 'https://qa-reports.linaro.org/api/projects/40/',
-    'linux-stable-rc-4.9-oe': 'https://qa-reports.linaro.org/api/projects/23/',
-    'linux-stable-rc-4.14-oe': 'https://qa-reports.linaro.org/api/projects/58/',
-    'linux-stable-rc-4.16-oe': 'https://qa-reports.linaro.org/api/projects/101/',
-}
-
 branches = {
     '4.4': [
-        'https://qa-reports.linaro.org/api/projects/34/',
         'https://qa-reports.linaro.org/api/projects/40/',
+        'https://qa-reports.linaro.org/api/projects/34/',
     ],
     '4.9': ['https://qa-reports.linaro.org/api/projects/23/'],
     '4.14': ['https://qa-reports.linaro.org/api/projects/58/'],
@@ -30,10 +22,17 @@ branches = {
 
 }
 
-for url in branches[branch]:
+for i, url in enumerate(branches[branch]):
     r = requests.get(url+'builds')
     result = r.json()['results'][0]
     r = requests.get(result['url']+'email?template=9')
-    print(r.text)
+    text = r.text
+    if len(branches[branch]) > 1 and i != len(branches[branch])-1:
+        # Remove the last 3 line (sig) if there are more reports
+        # coming
+        text = '\n'.join(text.split('\n')[:-3])
+    #if i > 0:
+    #    text = '\n'.join(text.split('\n')[3:])
+    print(text)
 
 
