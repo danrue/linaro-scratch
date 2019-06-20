8090c4ea167cb4c94437f3e0d9a8cc24f2fb5393
Dan Rue <dan.rue@linaro.org>
2019-06-20 16:29:50 -0500

    Fix graph #4
    Signed-off-by: Dan Rue <dan.rue@linaro.org>

--- work/grafana/direct.sh
+++ work/grafana/direct.sh
@@ -3,7 +3,7 @@ mkdir -p grafana
 curl -H "Authorization: Bearer $(cat $HOME/.config/grafana_key)" 'https://grafana.linaro.org/render/d-solo/2co7gfmZk/lkft?tab=queries&orgId=1&var-group=lkft&var-branch=All&panelId=13&width=600&height=200' --output grafana/over_x_tests_run.png
 curl -H "Authorization: Bearer $(cat $HOME/.config/grafana_key)" 'https://grafana.linaro.org/render/d-solo/2co7gfmZk/lkft?tab=queries&orgId=1&var-group=lkft&var-branch=All&from=1497989364226&panelId=11&width=1000&height=500' --output grafana/tests_run_simple.png
 curl -H "Authorization: Bearer $(cat $HOME/.config/grafana_key)" 'https://grafana.linaro.org/render/d-solo/2co7gfmZk/lkft?orgId=1&var-group=lkft&var-branch=All&from=1497987169327&to=1561059169328&panelId=9&width=1000&height=500&tz=America%2FChicago' --output grafana/total_tests.png
-curl -H "Authorization: Bearer $(cat $HOME/.config/grafana_key)" 'https://grafana.linaro.org/render/d-solo/2co7gfmZk/lkft?orgId=1&var-group=lkft&var-branch=All&from=1497987240920&to=1561059240920&panelId=11&width=1000&height=500&tz=America%2FChicago' --output grafana/total_tests_by_category.png
+curl -H "Authorization: Bearer $(cat $HOME/.config/grafana_key)" 'https://grafana.linaro.org/render/d-solo/2co7gfmZk/lkft?tab=visualization&orgId=1&from=1497989364226&var-group=lkft&var-branch=All&panelId=14&width=1000&height=500' --output grafana/total_tests_by_category.png
 
 cat << EOF > grafana/index.html
 <head></head>
@@ -15,6 +15,8 @@ cat << EOF > grafana/index.html
 <img src="total_tests.png" />
 <br />
 <img src="total_tests_by_category.png" />
+<br />
+Generated with <a href="https://github.com/danrue/linaro-scratch/blob/master/work/grafana/direct.sh">https://github.com/danrue/linaro-scratch/blob/master/work/grafana/direct.sh</a>
 </body>
 EOF
 
