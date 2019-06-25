41461658bb325e279a3ace61bd565d1e3d9177ea
Dan Rue <dan.rue@linaro.org>
2019-06-25 15:21:24 -0500

    Combine arch frags into an lkft frag
    Signed-off-by: Dan Rue <dan.rue@linaro.org>

--- work/KV-152/lkft-fragments/gen_arch_frags.sh
+++ work/KV-152/lkft-fragments/gen_arch_frags.sh
@@ -2,3 +2,4 @@ cat */arm.frag | sort | uniq > arm.frag
 cat */x86_64.frag | sort | uniq > x86_64.frag
 cat */i386.frag | sort | uniq > i386.frag
 cat */arm64.frag | sort | uniq > arm64.frag
+cat *.frag | sort | uniq > lkft.frag
