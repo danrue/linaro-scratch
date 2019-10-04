4eff7a16462a5d94e19fd1ecf9704226b5f3c293
Dan Rue <dan.rue@linaro.org>
2019-10-04 09:42:48 -0500

    Use rc1 for 4.19
    Signed-off-by: Dan Rue <dan.rue@linaro.org>

--- stable-rc/generate_reports.sh
+++ stable-rc/generate_reports.sh
@@ -3,6 +3,6 @@ mkdir -p $DATE
 generate_lts_report.py 4.4 > $DATE/4.4 &
 generate_lts_report.py 4.9 > $DATE/4.9 &
 generate_lts_report.py --unfinished 4.14 > $DATE/4.14 &
-generate_lts_report.py --unfinished 4.19 > $DATE/4.19 &
+generate_lts_report.py --unfinished --build 23320 4.19 > $DATE/4.19 & # build v4.19.76-212-g319532606385
 generate_lts_report.py 5.2 > $DATE/5.2 &
 generate_lts_report.py 5.3 > $DATE/5.3 &
